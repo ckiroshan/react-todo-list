c5f154ea13abb8f0ab5eaa7d3abd7a034b703856
ckiroshan <ckiroshan@gmail.com>
2024-07-20 13:12:29 +0530

    feat: update App.jsx to integrate routing and user authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,41 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { SignedIn, UserButton, useUser } from "@clerk/clerk-react";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Auth from "./pages/auth/Auth";
 
 function App() {
-
+  const { user } = useUser();
   return (
-    <>
-      <h1>Hey!</h1>
-    </>
-  )
+    <Router>
+      <div className="app-container">
+        <div className="navbar">
+          <div className="logo">
+            <Link to="/">
+              Todo Tracker
+            </Link>
+          </div>
+          <div className="navbar-center">
+            <SignedIn>
+              <Link to="/dashboard">Balance Insight</Link>
+            </SignedIn>
+          </div>
+          <div className="navbar-right">
+            <SignedIn>
+              <div className="navbar-greeting">
+                Hi, <span className="heading-username">{user?.firstName}!</span>
+              </div>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </div>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Auth />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
